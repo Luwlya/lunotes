cca627e68d2b4cffb367e0dc6831f05f0f96b727
Lu Kotova <loui.kotova@gmail.com>
2023-10-08 22:53:49 +0200

    initial commit

---
+++ src/main/java/com/luwlya/lunotes/LunotesApplication.java
@@ -0,0 +1,13 @@
+package com.luwlya.lunotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luwlya/lunotes/LunotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luwlya.lunotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
