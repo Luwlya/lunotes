ccba51218f623771fe69233310775d37fd9d1656
Lu Kotova <loui.kotova@gmail.com>
2023-10-08 23:21:11 +0200

    add health endpoint and docker compose

---
+++ src/main/java/com/luwlya/lunotes/http/HealthController.java
@@ -0,0 +1,12 @@
+package com.luwlya.lunotes.http;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Healthy";
+    }
+}
