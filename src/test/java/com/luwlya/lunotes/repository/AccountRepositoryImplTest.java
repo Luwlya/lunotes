2db07402b5a4be12ba46f464620213256ed293ff
Lu Kotova <loui.kotova@gmail.com>
2024-02-20 23:28:19 +0100

    add test for AccountRepository

--- src/test/java/com/luwlya/lunotes/repository/AccountRepositoryImplTest.java
+++ src/test/java/com/luwlya/lunotes/repository/AccountRepositoryImplTest.java
@@ -1,15 +1,45 @@
 package com.luwlya.lunotes.repository;
 
+import com.luwlya.lunotes.model.Account;
+import com.luwlya.lunotes.model.AccountStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AccountRepositoryImplTest {
+    private AccountRepositoryImpl unit;
+
+    @BeforeEach
+    void setUp() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5431/lunotes");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        unit = new AccountRepositoryImpl(jdbcTemplate);
+        jdbcTemplate.update("TRUNCATE TABLE accounts CASCADE");
+    }
 
     @Test
     void insert() {
+        OffsetDateTime utcNow = OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC);
+        Account account = new Account(UUID.randomUUID(), "Headshot", "head@cat.com", "hash", utcNow, utcNow.plusSeconds(1), AccountStatus.ACTIVE);
+        unit.insert(account);
+        Account actualAccount = unit.get(account.id());
+        assertEquals(account, actualAccount);
     }
 
     @Test
     void get() {
+
     }
 
     @Test
